69ea42ebd0cb3736ad3e195ddb95ee1ae2245327
Denis <corvus007@yandex.ru>
2018-01-09 12:57:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# AmoCRM Widget Starter Kit
+A skeleton of an amocrm widget example (https://developer.amocrm.com/widgets/) with Gulp builder
+
+## Build script:
+* JS files include (gulp-include)
+* Remove commets (required for publish)
+* Bump manifest version (required for load new version)
+* JSON validator - JSONLint
+* Javascript errors check - JSHint
+* CSS Autoprefixer
+* CSS errors check - CSSLint
+* Create zip archive to publish
+
+## Getting Started
+
+#### 1. Requires Node and NPM [Download and install node.js](http://nodejs.org/download/).
+
+#### 2. Clone this repository
+
+#### 3. Install gulp
+
+See https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md
+
+#### 4. Install npm dependencies
+
+```sh
+$ npm install
+```
+
+#### 5. Build
+```sh
+$ npm run build
+```
---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  jshint = require('gulp-jshint'),
+  jsonlint = require('gulp-jsonlint'),
+  csslint = require('gulp-csslint'),
+  zip = require('gulp-zip'),
+  include = require('gulp-include'),
+  autoprefixer = require('gulp-autoprefixer'),
+  strip = require('gulp-strip-comments'),
+  del = require('del'),
+  pkg = require('./package'),
+  manifest = require('./src/manifest'),
+  jmodify = require('gulp-json-modify'),
+  semver = require('semver');
+
+var path = {
+    src: './src/',
+    dist: './dist/',
+    publish: './publish/'
+  },
+  zipFileName = 'widget.zip';
+
+gulp.task('clean', function() {
+  return del([path.dist + '/**', path.publish + '/**']);
+});
+
+gulp.task('img', ['clean'], function() {
+  return gulp
+    .src(path.src + 'images/**')
+    .pipe(gulp.dest(path.dist + 'images/'));
+});
+
+gulp.task('templates', ['clean'], function() {
+  return gulp
+    .src(path.src + 'templates/**')
+    .pipe(gulp.dest(path.dist + 'templates/'));
+});
+
+gulp.task('jslint', ['clean'], function() {
+  return gulp
+    .src(path.src + 'js/*.js')
+    .pipe(jshint(pkg.jshintConfig))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('js', ['jslint'], function() {
+  return gulp
+    .src(path.src + 'js/script.js')
+    .pipe(include())
+    .on('error', console.log)
+    .pipe(strip())
+    .pipe(gulp.dest(path.dist));
+});
+
+gulp.task('i18n', ['clean'], function() {
+  return gulp
+    .src(path.src + 'i18n/*.json')
+    .pipe(jsonlint())
+    .pipe(jsonlint.failOnError())
+    .pipe(jsonlint.reporter())
+    .pipe(gulp.dest(path.dist + 'i18n/'));
+});
+
+gulp.task('css', ['clean'], function() {
+  return gulp
+    .src(path.src + 'css/style.css')
+    .pipe(csslint())
+    .pipe(csslint.formatter('compact'))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(path.dist));
+});
+
+gulp.task('manifest', ['js', 'i18n', 'css', 'img', 'templates'], function() {
+  return gulp
+    .src(path.src + 'manifest.json')
+    .pipe(jsonlint())
+    .pipe(jsonlint.failOnError())
+    .pipe(jsonlint.reporter())
+    .pipe(
+      jmodify({
+        key: 'widget.version',
+        value: semver.inc(manifest.widget.version, 'patch', null)
+      })
+    )
+    .pipe(gulp.dest(path.src))
+    .pipe(gulp.dest(path.dist));
+});
+
+gulp.task('zip', ['manifest'], function() {
+  return gulp
+    .src(path.dist + '**/*.*')
+    .pipe(zip(zipFileName))
+    .pipe(gulp.dest(path.publish));
+});
+
+gulp.task('build', ['zip'], function() {
+  gutil.log(
+    gutil.colors.green('Successfull build: '),
+    gutil.colors.cyan(path.publish + zipFileName)
+  );
+});
+
+gulp.task('default', ['build']);
